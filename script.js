42524624f0f2fd699cc010ba86dd1151b82e5e59
aliawaleed <57350259+aliawaleed@users.noreply.github.com>
2021-05-03 08:57:47 +0400

    updated website with icons and mapping to sides

--- script.js
+++ script.js
@@ -1,80 +1,44 @@
+/*to animate and spin cube on scroll*/
 window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
 
-// let panelContainers = document.getElementsByClassName("panel")
-// let panelContainersArray = []
-//
-// let nav
-//
-// let panel = null
-// let currInterval = -1
-// let yCoord
-//
-// const keyPressedHandler = (buttonCode) => {
-//     console.log(buttonCode)
-//     const num1KeyCode = 49
-//     panel = panelContainersArray[buttonCode - num1KeyCode]
-//     yCoord = panel.getBoundingClientRect().top
-//
-//     // if there currently an interval set remove it
-//     if (currInterval!=-1){
-//         clearInterval(currInterval)
-//         currInterval = -1
-//     }
-//
-//     if (yCoord > 0){ // is the panel is not at the top
-//         currInterval = setInterval(() => {
-//             // console.log(panel.getBoundingClientRect().top)/
-//             if (panel.getBoundingClientRect().top>=0) {
-//                 if (panel.getBoundingClientRect().top<10) {
-//                     window.scrollBy(0,panel.getBoundingClientRect().top)
-//                     clearInterval(currInterval)
-//                     currInterval = -1
-//                 }
-//                 window.scrollBy(0,10)
-//             } else {
-//                 clearInterval(currInterval)
-//                 currInterval = -1
-//             }
-//         })
-//     } else if(yCoord < 0){
-//         currInterval = setInterval(() => {
-//             // console.log(panel.getBoundingClientRect().top)
-//             if (panel.getBoundingClientRect().top<=0) {
-//                 if (panel.getBoundingClientRect().top>-10) {
-//                     window.scrollBy(0,panel.getBoundingClientRect().top)
-//                     clearInterval(currInterval)
-//                     currInterval = -1
-//                 }
-//                 window.scrollBy(0,-10)
-//             } else {
-//                 clearInterval(currInterval)
-//                 currInterval = -1
-//             }
-//         })
-//         // console.log("panel is above")
-//     }
-//     // panel.scrollIntoView()
-// }
-//
-// document.addEventListener("keypress",(e) => keyPressedHandler(e.keyCode))
-//
-// Object.keys(panelContainers).forEach(panelContainerKey => {
-//     console.log(window.innerHeight)
-//     let panel = panelContainers[panelContainerKey]
-//     panel.style.height = window.innerHeight+"px"
-//     panelContainersArray.push(panel)
-// })
-//
-// // event listeners for nav items
-// const panelOneNav = document.getElementById("1")
-// const panelTwoNav = document.getElementById("2")
-// const panelThreeNav = document.getElementById("3")
-// const panelFourNav = document.getElementById("4")
-//
-// // panel one arg is 49 panel 6 arg is 54
-// panelOneNav.addEventListener("click",() => keyPressedHandler(49))
-// panelTwoNav.addEventListener("click",() => keyPressedHandler(50))
-// panelThreeNav.addEventListener("click",() => keyPressedHandler(51))
-// panelFourNav.addEventListener("click",() => keyPressedHandler(52))
+/*all of the code below maps the icons to their respective side of the cube*/
+let currentInterval = null
+
+const navButtons = document.getElementsByClassName("navBtn")
+
+for (let i=0; i < navButtons.length; i++) {
+  navButtons[i].addEventListener("click", () => scrollFunction(i) )
+}
+
+const scrollFunction = (btn) => {
+  const targetPosition = (document.body.scrollHeight/navButtons.length)*btn
+
+  console.log(window.pageYOffset)
+
+  if (currentInterval != null){
+    clearInterval(currentInterval)
+    currentInterval = null
+  }
+
+  currentInterval = setInterval(() => {
+    const currentScrollPosition = window.pageYOffset
+    if (targetPosition > currentScrollPosition) {
+      if (targetPosition - currentScrollPosition < 10) {
+        window.scrollBy(0, targetPosition - currentScrollPosition)
+        clearInterval(currentInterval)
+        currentInterval = null
+      } else {
+        window.scrollBy(0,10)
+      }
+    } else {
+      if (currentScrollPosition - targetPosition < 10) {
+        window.scrollBy(0, currentScrollPosition - targetPosition)
+        clearInterval(currentInterval)
+        currentInterval = null
+      }
+      window.scrollBy(0,-10)
+    }
+  })
+}
